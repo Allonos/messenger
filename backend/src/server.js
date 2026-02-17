0c2db43bba75bc43e8335f391475035817933782
rezi tsitsvidze <r.tsitsvidze@optimogroup.io>
2026-02-17 18:12:55 +0400

    feat: ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,8 +10,18 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join("../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(3000, () => console.log(`Server is running on port ${PORT}`));
