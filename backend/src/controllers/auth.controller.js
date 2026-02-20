7a977ba6885bc62cecb13c9716eb9d59758ce305
rezi tsitsvidze <r.tsitsvidze@optimogroup.io>
2026-02-20 16:27:25 +0400

    feat: arcjet rate limiting implementation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
-import { ENV } from "../../lib/env.js";
-import { generateToken } from "../../lib/utils.js";
+import { ENV } from "../lib/env.js";
+import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import cloudinary from "../../lib/cloudinary.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "../lib/db.js";
-import { ENV } from "../lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 const app = express();
 const __dirname = path.resolve();
